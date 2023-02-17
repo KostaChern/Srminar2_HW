6318dd65c638c3ecc58522b03ba542d2ea833b98
Konstantin Chern <codahl@yandex.ru>
2023-02-17 14:50:51 +0300

    go solving Task13

---
+++ Task13/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа
+// или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Clear();
+int Promt(string msg)
+{
+    System.Console.WriteLine($"{msg}");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int num = Promt(" Введите число >");
