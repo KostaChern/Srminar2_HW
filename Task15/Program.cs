73c63b51bcb4e3d03021ebb75923008305ce9138
Konstantin Chern <codahl@yandex.ru>
2023-02-19 00:46:42 +0300

    Task15 solved

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -4,3 +4,16 @@
 // 7 -> да
 // 1 -> нет
 
+Console.Clear();
+int num = new Random().Next(1, 8);
+System.Console.WriteLine(num);
+
+if (num < 6)
+{
+    System.Console.WriteLine($" {num} день недели не является выходным");
+}
+
+else
+{
+    System.Console.WriteLine($" {num} день недели  ВЫХОДНОЙ!!");
+}
